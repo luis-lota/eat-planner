8ff40592c293d3bb7611acfcfb9dd784a70c80ca
luis-lota <louis.miguel.05@gmail.com>
2024-03-03 11:03:57 +0000

    adding routing for dashboard without validation of login

--- eat-planner/src/app/app.routes.ts
+++ eat-planner/src/app/app.routes.ts
@@ -1,11 +1,18 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from '../auth/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
     {
         path: 'login',
         title: 'Eat planner-login',
         component: LoginComponent
+    },
+
+    {
+        path: 'dashboard',
+        title: 'Eat planner-dashboard',
+        component: DashboardComponent
     },
 
     {
